de0f959dfa3983cd2a5d015b7daa4716e5150187
bliiir <rasgroth@gmail.com>
2019-07-04 16:08:26 +0200

    Solved lab 15_01

--- 15_variable_arguments/15_01_args.py
+++ 15_variable_arguments/15_01_args.py
@@ -2,3 +2,13 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def my_args(*args):
+    '''Print out the arguments received
+    '''
+    print('These are the arguments you gave me: \n')
+    for arg in args:
+        print(arg)
+
+# Call the my_args function with a string, a number and a copy of itself
+unsure('a string', 4, my_args)
