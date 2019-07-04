385d69f6096a8912cdf307f3c053e406f821f520
bliiir <rasgroth@gmail.com>
2019-07-04 16:33:25 +0200

    Solved lab 15_02

--- 15_variable_arguments/15_02_kwargs.py
+++ 15_variable_arguments/15_02_kwargs.py
@@ -2,3 +2,19 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def my_kwargs(**kwargs):
+    '''Print out the arguments received
+    '''
+    print('\nThese are the arguments you gave me: \n')
+    print('First as a dictionary: ', kwargs)
+    print('\nThen individually:\n')
+    for k, v in kwargs.items():
+        print(f'{k:<15s}: {v:}')
+
+# Call the my_args function with a string, a number and a copy of itself
+my_kwargs(
+    my_string='Hello!', 
+    my_number=4, 
+    my_list=[1,2,4], 
+    my_function=my_kwargs)
